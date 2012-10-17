2fb2bb88b900dee867d8ef1317ccf90cfd7f6311
TY@MacBook Pro <admin@pythoner.net>
2012-10-17 17:22:28 +0800

    上传到了pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+#encoding:utf-8
 from distutils.core import setup  
 setup(name='DjangoVerifyCode',  
       author='TY',  
       author_email='tianyu0915@gmail.com',  
       version='0.1',  
-      description='Django verify code',  
-      keywords ='html5 slide generator',
+      description='django验证码',  
+      keywords ='django verify code 验证码',
       #install_requires=["Django>=1.0","PIL>=1.1"],
       url='http://github.com/tianyu0915',  
       packages=['DjangoVerifyCode'],  
