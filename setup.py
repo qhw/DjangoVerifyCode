89b433eb178f521d78956809b160bda958bfbb9e
TY@MacBook Pro <admin@pythoner.net>
2012-10-19 13:44:09 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='DjangoVerifyCode',  
       author='TY(pythoner.net)',  
       author_email='tianyu0915@gmail.com',  
-      version='0.2.0',  
+      version='0.2.2',  
       description='在django中生成英文/数字公式验证码',  
       keywords ='django verify code 验证码',
       url='http://github.com/tianyu0915/DjangoVerifyCode',  
